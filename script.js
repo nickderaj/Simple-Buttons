ec6300df3c1a8270c1b5f55dc1e7e97eec50338e
Nick De Raj <nickderaj@gmail.com>
2021-06-29 12:43:30 +0800

    Basic CSS & HTML & Selecting vars in JS

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const buttons = document.querySelectorAll('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButtons = document.querySelector('.close-modal');
+const openButtons = document.querySelectorAll('.show-modal');
