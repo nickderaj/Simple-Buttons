1f386bfbae0832b2037402aa8390a1965305848e
Nick De Raj <nickderaj@gmail.com>
2021-06-29 12:46:07 +0800

    Hiding the overlay functionality

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-'use strict';
+"use strict";
 
-const buttons = document.querySelectorAll('.modal');
-const overlay = document.querySelector('.overlay');
-const closeButtons = document.querySelector('.close-modal');
-const openButtons = document.querySelectorAll('.show-modal');
+const buttons = document.querySelectorAll(".modal");
+const overlay = document.querySelector(".overlay");
+const closeButtons = document.querySelector(".close-modal");
+const openButtons = document.querySelectorAll(".show-modal");
+
+// Hide Overlay & Buttons:
+const addClasses = function () {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+};
+
+closeButtons.addEventListener("click", addClasses);
+overlay.addEventListener("click", addClasses);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    addClasses();
+  }
+});
